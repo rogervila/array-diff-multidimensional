d70b4d9ca1faea9baa010e7788b6a394b691636b
Roger Vilà <rogervila@me.com>
2021-07-23 11:39:45 +0200

    Create HelperFunctionTest.php

---
+++ tests/HelperFunctionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Rogervila\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class HelperFunctionTest extends TestCase
+{
+    /** @test */
+    public function it_returns_an_array()
+    {
+        $this->assertTrue(is_array(array_diff_multidimensional([], [])));
+    }
+
+    /** @test */
+    public function loose_comparisons()
+    {
+        $new = [
+            'a' => 'b',
+            'c' => 288,
+        ];
+
+        $old = [
+            'a' => 'b',
+            'c' => '288',
+        ];
+
+        $this->assertEquals(0, count(array_diff_multidimensional($new, $old, false)));
+        $this->assertFalse(isset(array_diff_multidimensional($new, $old, false)['c']));
+    }
+
+    /** @test */
+    public function strict_comparisons()
+    {
+        $new = [
+            'a' => 'b',
+            'c' => 288,
+        ];
+
+        $old = [
+            'a' => 'b',
+            'c' => '288',
+        ];
+
+        $this->assertEquals(1, count(array_diff_multidimensional($new, $old)));
+        $this->assertTrue(isset(array_diff_multidimensional($new, $old)['c']));
+        $this->assertEquals(288, array_diff_multidimensional($new, $old)['c']);
+
+        $this->assertEquals(1, count(array_diff_multidimensional($new, $old, true)));
+        $this->assertTrue(isset(array_diff_multidimensional($new, $old, true)['c']));
+        $this->assertEquals(288, array_diff_multidimensional($new, $old, true)['c']);
+    }
+}
