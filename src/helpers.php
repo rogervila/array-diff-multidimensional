9aa293671db9216719450c8a84547ce3d1051e14
Roger Vilà <rogervila@me.com>
2023-04-19 11:47:03 +0200

    PHPUnit 10 Support (#49)

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,6 @@
 use Rogervila\ArrayDiffMultidimensional;
 
 if (!function_exists('array_diff_multidimensional')) {
-
     /**
      * Returns an array with the differences between $array1 and $array2
      * $strict variable defines if comparison must be strict or not
