25e97c5a3341f03b46b3214e6194b950faf9251e
Roger Vilà <rogervila@me.com>
2025-09-12 13:06:19 +0200

    chore: Additional tests

---
+++ tests/ArrayDiffEdgeCasesTest.php
@@ -0,0 +1,278 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Rogervila\ArrayDiffMultidimensional;
+
+class ArrayDiffEdgeCasesTest extends TestCase
+{
+    /** @test */
+    public function it_handles_array_key_exists_vs_isset_edge_cases()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        // Test with null values that make isset return false but array_key_exists return true
+        $new = ['key' => null, 'another' => 'value'];
+        $old = ['key' => 'not_null', 'another' => 'value'];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals(['key' => null], $result);
+
+        // Test missing key vs null value
+        $new = ['existing' => null];
+        $old = [];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals(['existing' => null], $result);
+    }
+
+    /** @test */
+    public function it_handles_false_vs_zero_edge_cases()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $new = ['a' => false, 'b' => 0, 'c' => '', 'd' => null];
+        $old = ['a' => 0, 'b' => false, 'c' => null, 'd' => ''];
+
+        // Strict mode should detect all differences
+        $result = $diff->compare($new, $old, true);
+        $this->assertEquals($new, $result);
+
+        // Loose mode should detect fewer differences
+        $result = $diff->compare($new, $old, false);
+        $this->assertEquals([], $result);
+    }
+
+    /** @test */
+    public function it_handles_empty_string_vs_null_edge_cases()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $new = ['empty_string' => '', 'null_value' => null, 'zero' => 0];
+        $old = ['empty_string' => null, 'null_value' => '', 'zero' => false];
+
+        $result = $diff->compare($new, $old, true);
+        $this->assertEquals($new, $result);
+
+        $result = $diff->compare($new, $old, false);
+        $this->assertEquals([], $result);
+    }
+
+    /** @test */
+    public function it_handles_numeric_string_edge_cases()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $new = ['int' => 123, 'float' => 123.0, 'string' => '123', 'float_string' => '123.0'];
+        $old = ['int' => '123', 'float' => '123', 'string' => 123, 'float_string' => 123.0];
+
+        // Strict mode should detect type differences
+        $result = $diff->compare($new, $old, true);
+        $this->assertEquals($new, $result);
+
+        // Loose mode should ignore most numeric type differences
+        $result = $diff->compare($new, $old, false);
+        $this->assertEquals([], $result);
+    }
+
+    /** @test */
+    public function it_handles_resource_values()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $resource1 = tmpfile();
+        $resource2 = tmpfile();
+
+        $new = ['resource' => $resource1];
+        $old = ['resource' => $resource2];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals(['resource' => $resource1], $result);
+
+        // Same resource should not show difference
+        $new = ['resource' => $resource1];
+        $old = ['resource' => $resource1];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals([], $result);
+
+        fclose($resource1);
+        fclose($resource2);
+    }
+
+    /** @test */
+    public function it_handles_callable_values()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $callable1 = function() { return 'test1'; };
+        $callable2 = function() { return 'test2'; };
+
+        $new = ['callable' => $callable1];
+        $old = ['callable' => $callable2];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals(['callable' => $callable1], $result);
+
+        // Same callable should not show difference
+        $new = ['callable' => $callable1];
+        $old = ['callable' => $callable1];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals([], $result);
+    }
+
+    /** @test */
+    public function it_handles_array_vs_object_edge_cases()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $array = ['property' => 'value'];
+        $object = new \stdClass();
+        $object->property = 'value';
+
+        $new = ['item' => $array];
+        $old = ['item' => $object];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals(['item' => $array], $result);
+    }
+
+    /** @test */
+    public function it_handles_nested_array_vs_scalar_transitions()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $new = [
+            'a' => [
+                'b' => [
+                    'c' => 'scalar_value'
+                ]
+            ]
+        ];
+
+        $old = [
+            'a' => [
+                'b' => 'scalar_at_b_level'
+            ]
+        ];
+
+        $result = $diff->compare($new, $old);
+        $expected = [
+            'a' => [
+                'b' => [
+                    'c' => 'scalar_value'
+                ]
+            ]
+        ];
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function it_handles_very_large_float_precision()
+    {
+        $this->markTestSkipped('TODO: fix precision handling');
+        $diff = new ArrayDiffMultidimensional();
+
+        $precision = 1e-15;
+        $base = 1.23456789012345;
+
+        $new = ['precise' => $base];
+        $old = ['precise' => $base + $precision];
+
+        $result = $diff->compare($new, $old, true);
+        $this->assertEquals(['precise' => $base], $result);
+
+        // Test with extremely small differences that might be lost in string conversion
+        $new = ['tiny_diff' => 1.0000000000000001];
+        $old = ['tiny_diff' => 1.0000000000000002];
+
+        $result = $diff->compare($new, $old, true);
+        // Due to float precision limits, this might or might not show a difference
+        // The important thing is that it doesn't crash
+        $this->assertTrue(is_array($result));
+    }
+
+    /** @test */
+    public function it_handles_empty_arrays_at_different_nesting_levels()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $new = [
+            'level1' => [],
+            'level2' => [
+                'nested' => []
+            ],
+            'level3' => [
+                'deep' => [
+                    'deeper' => []
+                ]
+            ]
+        ];
+
+        $old = [
+            'level1' => ['not_empty'],
+            'level2' => [
+                'nested' => ['also_not_empty']
+            ],
+            'level3' => [
+                'deep' => [
+                    'deeper' => ['deepest_not_empty']
+                ]
+            ]
+        ];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals($new, $result);
+    }
+
+    /** @test */
+    public function it_handles_circular_reference_prevention()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        // Create objects with circular references
+        $obj1 = new \stdClass();
+        $obj2 = new \stdClass();
+        $obj1->ref = $obj2;
+        $obj2->ref = $obj1;
+
+        $obj3 = new \stdClass();
+        $obj4 = new \stdClass();
+        $obj3->ref = $obj4;
+        $obj4->ref = $obj3;
+
+        $new = ['circular' => $obj1];
+        $old = ['circular' => $obj3];
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals(['circular' => $obj1], $result);
+    }
+
+    /** @test */
+    public function it_handles_array_keys_with_special_characters()
+    {
+        $diff = new ArrayDiffMultidimensional();
+
+        $new = [
+            'normal_key' => 'value1',
+            'key with spaces' => 'value2',
+            'key.with.dots' => 'value3',
+            'key-with-dashes' => 'value4',
+            'key_with_underscores' => 'value5',
+            'key/with/slashes' => 'value6',
+            'key\\with\\backslashes' => 'value7',
+            'key:with:colons' => 'value8',
+            'key;with;semicolons' => 'value9',
+            'key=with=equals' => 'value10',
+            'key?with?questions' => 'value11',
+            'key&with&ampersands' => 'value12',
+            'key#with#hashes' => 'value13',
+            'key@with@ats' => 'value14'
+        ];
+
+        $old = array_fill_keys(array_keys($new), 'old_value');
+
+        $result = $diff->compare($new, $old);
+        $this->assertEquals($new, $result);
+    }
+}
